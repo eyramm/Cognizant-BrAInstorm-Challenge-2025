449ce33c290885f95972afb576c67fba76216a64
Eyramm <eyramm@Mac.localdomain>
2025-11-09 12:29:28 -0400

    updated default env vars

--- Codebase/app/config/constants.ts
+++ Codebase/app/config/constants.ts
@@ -1,2 +1,2 @@
 // API Configuration
-export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || '';
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'https://hjbdhmmzi2.us-east-1.awsapprunner.com';
